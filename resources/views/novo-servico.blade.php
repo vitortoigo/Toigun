8db38156c897cdfe13a4f009ef8096aad3961514
vitortoigo <104221112+vitortoigo@users.noreply.github.com>
2022-06-07 19:07:35 -0300

    Update novo-servico.blade.php

--- resources/views/novo-servico.blade.php
+++ resources/views/novo-servico.blade.php
@@ -4,7 +4,7 @@
 @section('content')
 
 <form method="post" action="{{ route('novo-servico.enviar') }}">
-    <div class="px-4 md:px-10 py-4 md:py-7 bg-gray-100 dark:bg-gray-700 rounded-tl-lg rounded-tr-lg">
+    <div class="px-4 md:px-10 py-4 md:py-7 bg-gray-700 rounded-tl-lg rounded-tr-lg">
         <div class="sm:flex items-center justify-between">
             <p tabindex="0" class="focus:outline-none text-base sm:text-lg md:text-xl lg:text-2xl font-bold leading-normal text-gray-800 dark:text-white">Cadastrar novo serviço</p>
             <div>
@@ -15,7 +15,7 @@
         </div>
     </div>
 
-    <div class="bg-white dark:bg-gray-800  shadow px-4 md:px-10 py-12 md:pt-7 overflow-y-auto">
+    <div class="bg-gray-800  shadow px-4 md:px-10 py-12 md:pt-7 overflow-y-auto">
 
         @if($errors->any())
         <div class="alert alert-danger">
